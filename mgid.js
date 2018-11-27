ad802326205c4d24b5ca61658c9b4ac8a7ecfb12
paderisiahaan <37983012+paderisiahaan@users.noreply.github.com>
2018-11-27 22:52:38 +0700

    Add files via upload

---
+++ mgid.js
@@ -0,0 +1,13 @@
+<!-- Composite Start -->
+<div id="M372910ScriptRootC289153">
+        <div id="M372910PreloadC289153">
+        Loading...    </div>
+        <script>
+                (function(){
+            var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
+            var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M372910ScriptRootC289153")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
+            catch(e){var iw=d;var c=d[gi]("M372910ScriptRootC289153");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=289153;c[ac](dv);
+            var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/a/y/ayonime.com.289153.js?t="+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();
+    </script>
+    </div>
+<!-- Composite End -->
